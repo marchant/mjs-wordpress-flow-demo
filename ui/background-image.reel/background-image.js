dcb10dc1610a12bab3e7874139c5e38cea4cb103
Benoit Marchant <benoit@montagestudio.com>
2015-06-04 17:14:09 +0000

    Add component background-image

---
+++ ui/background-image.reel/background-image.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/background-image.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class BackgroundImage
+ * @extends Component
+ */
+exports.BackgroundImage = Component.specialize(/** @lends BackgroundImage# */ {
+    constructor: {
+        value: function BackgroundImage() {
+            this.super();
+        }
+    }
+});
